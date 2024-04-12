19a1d76275f3f8ff53bcc12b040f0eae81687e46
ZacharyTrux <ztruxillo@gmail.com>
2024-04-12 10:59:46 -0500

    ghggh

--- Room Adventure/RoomAdventure-TEMPLATE.py
+++ Room Adventure/RoomAdventure-TEMPLATE.py
@@ -114,6 +114,7 @@ class Game(Frame):
 
 	# creates the rooms
 	def createRooms(self):
+		#change before we put in 
 		r1 = Room("Room 1", "Room Adventure/roomImage/room1.gif")
 		r2 = Room("Room 2", "Room Adventure/roomImage/room2.gif")
 		r3 = Room("Room 3", "Room Adventure/roomImage/room3.gif")
